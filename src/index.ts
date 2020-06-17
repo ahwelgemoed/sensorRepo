b260cd50f1fcc6544734e18a32122002765088e7
ahwelgemoed <arno.welgemoed@outlook.com>
2020-06-17 17:19:45 +0200

    removeTesting Data

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 import WebSocket from 'ws';
 import sensor from 'node-dht-sensor';
+import { setInterval } from 'timers';
 
 const asyncSensor = sensor.promises;
 
 const ws = new WebSocket('ws://10.0.0.126:8080');
 
 ws.on('open', function open() {
-  sentTempToSocket();
   ws.send('Im Online');
+  setInterval(() => {
+    sentTempToSocket();
+  }, 5000);
 });
+
 interface SensorValues extends Promise<void> {
   temperature: string;
   humidity: string;
 }
+
 const getTemp = async () => {
-  asyncSensor.initialize({
-    test: {
-      fake: {
-        temperature: 21,
-        humidity: 60,
-      },
-    },
-  });
   try {
     const res = await asyncSensor.read(22, 4);
     return res;
   } catch (err) {
+    const x = JSON.stringify(err);
+    await ws.send(x);
     console.error('Failed to read sensor data:', err);
   }
 };
