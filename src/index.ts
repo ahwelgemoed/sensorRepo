dabb9b90323a10fbc401a9555e4fb02f7f2a5bc0
ahwelgemoed <arno.welgemoed@outlook.com>
2020-06-17 16:40:37 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,31 @@ import sensor from 'node-dht-sensor';
 const asyncSensor = sensor.promises;
 
 const ws = new WebSocket('ws://10.0.0.126:8080');
-
-async function exec() {
+interface SensorValues extends Promise<void> {
+  temperature: string;
+  humidity: string;
+}
+const getTemp = async () => {
+  asyncSensor.initialize({
+    test: {
+      fake: {
+        temperature: 21,
+        humidity: 60,
+      },
+    },
+  });
   try {
     const res = await asyncSensor.read(22, 4);
-    ws.on('open', () => {
-      ws.send([res]);
-    });
+    return res;
+    // ws.on('open', function open() {
+    //   ws.send([res]);
+    // });
   } catch (err) {
     console.error('Failed to read sensor data:', err);
   }
-}
+};
 
-exec();
+getTemp().then((res) => console.log('res', res));
 
 ws.on('message', function incoming(data) {
   console.log(data);
